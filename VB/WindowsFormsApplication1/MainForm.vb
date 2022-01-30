eecbbe8d37d01276bcfbf437e8c8854cbf1cdabc
DevExpressExampleBot <fakeEmail>
2022-01-30 20:10:32 +0300

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/MainForm.vb
+++ VB/WindowsFormsApplication1/MainForm.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 #Region "#Reference"
 Imports DevExpress.Pdf
 Imports DevExpress.XtraBars
@@ -6,78 +5,75 @@ Imports DevExpress.XtraEditors
 Imports System.IO
 Imports System.Text
 Imports System.Windows.Forms
+
 ' ...
-#End Region ' #Reference
-
+#End Region  ' #Reference
 Namespace WindowsFormsApplication1
-	Partial Public Class MainForm
-		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
-			pdfViewer1.LoadDocument("..\..\document.pdf")
-			AddHandler repositoryItemButtonEdit3.ButtonClick, AddressOf repositoryItemButtonEdit3_ButtonClick
-		End Sub
+    Public Partial Class MainForm
+        Inherits Ribbon.RibbonForm
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+            pdfViewer1.LoadDocument("..\..\document.pdf")
+            AddHandler repositoryItemButtonEdit3.ButtonClick, AddressOf repositoryItemButtonEdit3_ButtonClick
+        End Sub
 
 #Region "#ExtractText"
-Private Function ExtractTextFromPDF(ByVal filePath As String) As String
-	Dim documentText As String = ""
-	Try
-		Using documentStream As New FileStream(filePath, FileMode.Open, FileAccess.Read)
-		Using documentProcessor As New PdfDocumentProcessor()
-			documentProcessor.LoadDocument(documentStream)
-			documentText = documentProcessor.Text
-		End Using
-		End Using
-	Catch
-	End Try
-	Return documentText
-End Function
-#End Region ' #ExtractText
+        Private Function ExtractTextFromPDF(ByVal filePath As String) As String
+            Dim documentText As String = ""
+            Try
+                Using documentStream As FileStream = New FileStream(filePath, FileMode.Open, FileAccess.Read)
+                    Using documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+                        documentProcessor.LoadDocument(documentStream)
+                        documentText = documentProcessor.Text
+                    End Using
+                End Using
+            Catch
+            End Try
 
+            Return documentText
+        End Function
 
+#End Region  ' #ExtractText
 #Region "#WordCount"
-Private Function WordCount(ByVal filePath As String, ByVal word As String) As Integer
-	Dim count As Integer = 0
-	Try
-		Using documentStream As New FileStream(filePath, FileMode.Open, FileAccess.Read)
-		Using documentProcessor As New PdfDocumentProcessor()
-			documentProcessor.LoadDocument(documentStream)
-			Do While documentProcessor.FindText(word).Status = PdfTextSearchStatus.Found
-				count += 1
-			Loop
-		End Using
-		End Using
-	Catch
-	End Try
+        Private Function WordCount(ByVal filePath As String, ByVal word As String) As Integer
+            Dim count As Integer = 0
+            Try
+                Using documentStream As FileStream = New FileStream(filePath, FileMode.Open, FileAccess.Read)
+                    Using documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+                        documentProcessor.LoadDocument(documentStream)
+                        While documentProcessor.FindText(word).Status = PdfTextSearchStatus.Found
+                            count += 1
+                        End While
+                    End Using
+                End Using
+            Catch
+            End Try
 
-	Return count
-End Function
-#End Region ' #WordCount
+            Return count
+        End Function
 
-		Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) Handles barButtonItem1.ItemClick
-			Dim path As String = pdfViewer1.DocumentFilePath
-			Dim textViewer As New TextViewerForm()
-			textViewer.textBox1.Text = ExtractTextFromPDF(path)
-			textViewer.ShowDialog()
-		End Sub
+#End Region  ' #WordCount
+        Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            Dim path As String = pdfViewer1.DocumentFilePath
+            Dim textViewer As TextViewerForm = New TextViewerForm()
+            textViewer.textBox1.Text = ExtractTextFromPDF(path)
+            textViewer.ShowDialog()
+        End Sub
 
-
-		Private Sub repositoryItemButtonEdit3_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs)
-			Dim path As String = pdfViewer1.DocumentFilePath
-			Dim word As String = (TryCast(sender, ButtonEdit)).Text
-
-			Dim message As New StringBuilder()
-			message.Append("The number of times the word """)
-			message.Append(word)
-			message.Append(""" appears in the PDF document: ")
-			message.Append(WordCount(path, word).ToString())
-
-			MessageBox.Show(message.ToString())
-		End Sub
-	End Class
+        Private Sub repositoryItemButtonEdit3_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs)
+            Dim path As String = pdfViewer1.DocumentFilePath
+            Dim word As String = TryCast(sender, ButtonEdit).Text
+            Dim message As StringBuilder = New StringBuilder()
+            message.Append("The number of times the word """)
+            message.Append(word)
+            message.Append(""" appears in the PDF document: ")
+            message.Append(WordCount(path, word).ToString())
+            Call MessageBox.Show(message.ToString())
+        End Sub
+    End Class
 End Namespace
-
-
--- VB/WindowsFormsApplication1/TextViewerForm.vb
+++ VB/WindowsFormsApplication1/TextViewerForm.vb
@@ -1,23 +1,19 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
 
 Namespace WindowsFormsApplication1
-	Partial Public Class TextViewerForm
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Me.Close()
-		End Sub
-	End Class
+    Public Partial Class TextViewerForm
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Close()
+        End Sub
+    End Class
 End Namespace
